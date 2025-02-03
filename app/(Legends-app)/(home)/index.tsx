a6e1de29691f16b66d29f6d953ea6ed34bf9bb63
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-02-03 18:23:18 -0300

    Fuentes funcionando

--- app/(Legends-app)/(home)/index.tsx
+++ app/(Legends-app)/(home)/index.tsx
@@ -1,10 +1,17 @@
 import { View } from 'react-native'
 import React from 'react'
 import { ThemedText } from '@/presentation/theme/components/ThemedText'
+import { useThemeColor } from '@/presentation/theme/hooks/useThemeColor'
 
 const HomeScreen = () => {
+
+  const primary = useThemeColor({}, 'primary')
+
   return (
-    <View>
+    <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
+      <ThemedText style={{fontFamily: 'KanitBold', color: primary}} >HomeScreen</ThemedText>
+      <ThemedText style={{fontFamily: 'KanitRegular'}} >HomeScreen</ThemedText>
+      <ThemedText style={{fontFamily: 'KanitThin'}} >HomeScreen</ThemedText>
       <ThemedText>HomeScreen</ThemedText>
     </View>
   )
